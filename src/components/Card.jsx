a66b3f43e236dc1da047b2f00e1d0a59add29d98
Charlie <160505188+oneandonlycharlie@users.noreply.github.com>
2024-11-03 01:12:39 +0800

    Build: make photo cliclable

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,48 +1,50 @@
 import { useEffect,useState } from 'react';
 import './Card.css'
 
+
 function Card(){
 
-const [testName, setName]=useState("test")
-    
+const [isClicked, switchClick]=useState(false)
+const [testPerson, setName]=useState({
+    name:'',
+    img:''
+})
+  
+
 useEffect(()=> {
     const fetchData = async ()=> {
     let response = await fetch('https://hp-api.onrender.com/api/characters');
-    console.log(response)
-    let data = await response.json();
+    let rawData = await response.json()
+    let data = rawData.filter(item => item["image"]!=="");
+    console.log(data);
 
-    let name = data[0]['name'];
-    console.log(name);
-    setName(name);
+    let index = Math.floor(Math.random()*data.length);
+    let name = data[index]['name'];
+    let img = data[index]['image'];
+
+    setName({
+        name: name,
+        img: img
+    });
     };
     fetchData();
-    },[])   
+    },[isClicked])   
 
     return (
         <div className="card">
+            <button
+                onClick={()=> switchClick(!isClicked)}
+            >Switch Card</button>
             <p>I am a card</p>
-            <p> {testName} from API</p>
+            <p> {testPerson.name} from API</p>
+            <img 
+                src={testPerson.img} 
+                alt={testPerson.name}
+                onClick={()=> switchClick(!isClicked)}
+            />
         </div>
     )
 }
-
-
-async function getName(){
-
-    let response = await('https://hp-api.onrender.com/api/characters');
-    let data = await response.json();
-
-    let name = data[0]['name']
-
-    return name
-}
-
-
-
-
-
-
-
 
 
 export {Card}
